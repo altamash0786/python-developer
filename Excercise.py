c1bc311c01f4b2f10c9afdd011cb0293f682d6c2
Altamash raza <85454038+altamash0786@users.noreply.github.com>
2024-09-03 18:40:46 +0530

    Add files via upload

---
+++ Excercise.py
@@ -0,0 +1,12 @@
+n = 12
+m = 5
+ans1 = n+m
+print("Addition of",n,"and",m,"is",ans1)
+ans1 = n-m
+print("Substraction of",n,"and",m,"is",ans1)
+ans1 = n*m
+print("Multiplication of",n,"and",m,"is",ans1)
+ans1 = n/m
+print("division of",n,"and",m,"is",ans1)
+ans1 = n%m
+print("Modulus of",n,"and",m,"is",ans1)
